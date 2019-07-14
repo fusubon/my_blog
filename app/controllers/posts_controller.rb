552bd197b5eee46c0724e485ce80aa63d875bbc4
fusubon <info@fusubon.com>
2019-07-14 11:00:40 +0900

    削除機能まで完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,50 @@
 class PostsController < ApplicationController
+  def index
+    # 記事一覧用
+    @posts = Post.all
+    # 最新記事用
+    @new_posts = Post.all
+  end
+
+  def new
+    @post = Post.new # フォーム用の空のインスタンスを生成する。
+  end
+
+  def create
+    @post = Post.new(post_params) # ストロングパラメータを引数に
+    if @post.save
+      redirect_to @post, notice: "ブログを登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "ブログを更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "ブログを削除しました。"
+  end
+
+  private
+
+  def post_params # ストロングパラメータを定義する
+    params.require(:post).permit(:title, :body)
+  end 
 end
