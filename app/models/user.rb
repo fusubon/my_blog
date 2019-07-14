49f588537b98b917bca2c331875c3e45112e2ef9
fusubon <info@fusubon.com>
2019-07-14 16:11:27 +0900

    Userlogin added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+  has_many :posts
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :description,  presence: true, length: { maximum: 500 }
 end
--- db/migrate/20190714041054_add_category_id_to_posts.rb
+++ db/migrate/20190714041054_add_category_id_to_posts.rb
@@ -1,5 +1,5 @@
 class AddCategoryIdToPosts < ActiveRecord::Migration[5.2]
   def change
-    add_reference :posts, :category, foreign_key: true, index: true, after: :body
+    add_reference :posts, :user, foreign_key: true, index: true, after: :category_id
   end
 end
