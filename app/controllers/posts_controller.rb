8448c2c6901d4829c5f274618976c663707d863a
fusubon <info@fusubon.com>
2019-07-14 11:59:45 +0900

    リファクタリング終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
+
   def index
-    # 記事一覧用
-    @posts = Post.all
-    # 最新記事用
-    @new_posts = Post.all
+    @posts = Post.order(created_at: :desc)
+    @new_posts = Post.order(created_at: :desc).limit(5)
   end
 
   def new
@@ -20,15 +20,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: "ブログを更新しました。"
     else
@@ -37,7 +34,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: "ブログを削除しました。"
   end
@@ -46,5 +42,9 @@ class PostsController < ApplicationController
 
   def post_params # ストロングパラメータを定義する
     params.require(:post).permit(:title, :body)
-  end 
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
